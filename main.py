76a3bc1c5861249b31a32400cc63bcec13b2103f
geniousabby <abbyspam1228@gmail.com>
2026-03-04 14:22:25 -0800

    Add guest function
    Made an add guest function. Inputs name and adds name to the guest_name list.

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+"""
+Dinner guest list.
 
-# Add guest function
+- Add guest
+- Remove Guest
+- Show invitations
+
+"""
+
+guest_name = []
+
+
+def add_guest(): # Add guest function
+    """Add one guest and their name to the list."""
+
+    name = input("Enter guest name: ").strip().title()
+
+    guest_name.append(name)
 
 
 # Modify guest function
