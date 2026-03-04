48259f12b2d2d0cc8e6a8d5152b7b8f04e5a7134
geniousabby <abbyspam1228@gmail.com>
2026-03-04 13:48:44 -0800

    Main file, pseudocode
    Created a main file and added pseudocode.

---
+++ main.py
@@ -0,0 +1,24 @@
+
+# Add guest function
+
+
+# Modify guest function
+
+
+# Remove guest function
+
+
+# Sort guests function
+
+
+# Show number of guests function
+
+
+# Show invitations function
+
+
+# Duplicate checker function
+
+
+# Main function
+
